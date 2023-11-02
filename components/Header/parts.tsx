b2ae18b9bc7cb107135f63c2c08144da76efb002
elijahthis <emperorelijah007@gmail.com>
2023-11-02 20:56:20 +0100

    feat: user popup

--- components/Header/parts.tsx
+++ components/Header/parts.tsx
@@ -1,6 +1,13 @@
 "use client";
 import Link from "next/link";
 import {
+	Dispatch,
+	ReactNode,
+	SetStateAction,
+	cloneElement,
+	useState,
+} from "react";
+import {
 	AnalyticsIcon,
 	AppsIcon,
 	CRMIcon,
@@ -10,9 +17,13 @@ import {
 	NotificationIcon,
 	RevenueIcon,
 } from "@/components/svgs";
-import { cloneElement } from "react";
 import { usePathname } from "next/navigation";
 import Popup from "../Popup";
+import { PiSignOutLight } from "react-icons/pi";
+import { AiOutlineSetting } from "react-icons/ai";
+import { MdOutlineBugReport, MdOutlineSwitchAccount } from "react-icons/md";
+import { GoGift } from "react-icons/go";
+import { HiOutlineNewspaper } from "react-icons/hi";
 
 export const HeaderNavigation = () => {
 	const navList: { label: string; link: string; icon: JSX.Element }[] = [
@@ -52,34 +63,57 @@ export const HeaderNavigation = () => {
 	);
 };
 
-export const HeaderUser = () => (
-	<div className="flex flex-row items-center gap-2 relative ">
-		<div className="w-10 h-10 grid place-items-center cursor-pointer">
-			<NotificationIcon />
-		</div>
-		<div className="w-10 h-10 grid place-items-center cursor-pointer">
-			<ChatIcon />
-		</div>
-		<div className="rounded-[100px] bg-[#EFF1F6] flex flex-row items-center gap-2 py-1 pl-[5px] pr-3 cursor-pointer ">
-			<div
-				className="w-8 h-8 grid place-items-center rounded-full text-white text-sm "
-				style={{
-					background: "linear-gradient(139deg, #5C6670 2.33%, #131316 96.28%)",
-				}}
-			>
-				OJ
-			</div>
-			<HamburguerIcon />
-		</div>
-		{/* <HeaderUserDrop /> */}
-	</div>
-);
+export const HeaderUser = () => {
+	const [isOpen, setIsOpen] = useState(false);
 
-export const HeaderUserDrop = () => {
 	return (
-		<Popup>
+		<div className="flex flex-row items-center gap-2 relative ">
+			<div className="w-10 h-10 grid place-items-center cursor-pointer">
+				<NotificationIcon />
+			</div>
+			<div className="w-10 h-10 grid place-items-center cursor-pointer">
+				<ChatIcon />
+			</div>
+			<div
+				className="rounded-[100px] bg-[#EFF1F6] flex flex-row items-center gap-2 py-1 pl-[5px] pr-3 cursor-pointer "
+				onClick={() => setIsOpen((val) => !val)}
+			>
+				<div
+					className="w-8 h-8 grid place-items-center rounded-full text-white text-sm "
+					style={{
+						background:
+							"linear-gradient(139deg, #5C6670 2.33%, #131316 96.28%)",
+					}}
+				>
+					OJ
+				</div>
+				<HamburguerIcon />
+			</div>
+			<HeaderUserDrop isOpen={isOpen} setIsOpen={setIsOpen} />
+		</div>
+	);
+};
+
+export const HeaderUserDrop = ({
+	isOpen,
+	setIsOpen,
+}: {
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+}) => {
+	const popList: { icon: JSX.Element; label: string }[] = [
+		{ icon: <AiOutlineSetting />, label: "Settings" },
+		{ icon: <HiOutlineNewspaper />, label: "Purchase History" },
+		{ icon: <GoGift />, label: "Refer and Earn" },
+		{ icon: <AppsIcon />, label: "Integrations" },
+		{ icon: <MdOutlineBugReport />, label: "Report Bug" },
+		{ icon: <MdOutlineSwitchAccount />, label: "Switch Account" },
+		{ icon: <PiSignOutLight />, label: "Sign Out" },
+	];
+	return (
+		<Popup isOpen={isOpen} setIsOpen={setIsOpen}>
 			<div>
-				<div>
+				<div className="flex flex-row items-center gap-3 px-5 py-5">
 					<div
 						className="w-8 h-8 grid place-items-center rounded-full text-white text-sm "
 						style={{
@@ -90,10 +124,21 @@ export const HeaderUserDrop = () => {
 						OJ
 					</div>
 					<div>
-						<p>Olivier Jones</p>
-						<p>olivierjones@gmail.com</p>
+						<p className="text-lg font-semibold text-[#131316] ">
+							Olivier Jones
+						</p>
+						<p className="text-sm">olivierjones@gmail.com</p>
 					</div>
 				</div>
+				{popList.map((item, ind) => (
+					<div
+						className="flex flex-row items-center gap-4 py-4 px-6 cursor-pointer bg-white hover:bg-[#131316] hover:text-white "
+						onClick={() => setIsOpen(false)}
+					>
+						<span>{item.icon}</span>
+						<span>{item.label}</span>
+					</div>
+				))}
 			</div>
 		</Popup>
 	);
--- components/Popup/index.tsx
+++ components/Popup/index.tsx
@@ -1,7 +1,33 @@
-import { ReactNode } from "react";
+"use client";
+import useOutsideAlerter from "@/hooks/useOutsideAlerter";
+import { Dispatch, ReactNode, SetStateAction, useRef, useState } from "react";
 
-const Popup = ({ children }: { children: ReactNode }) => {
-	return <div className="popUp">{children}</div>;
+interface PopupProps {
+	children: ReactNode;
+	isOpen: boolean;
+	setIsOpen: Dispatch<SetStateAction<boolean>>;
+}
+
+const Popup = ({ children, isOpen, setIsOpen }: PopupProps) => {
+	const wrapperRef = useRef(null);
+
+	useOutsideAlerter(wrapperRef, () => {
+		setIsOpen(false);
+	});
+
+	return (
+		<div
+			className={`popUp w-[400px] overflow-hidden ${isOpen ? "h-max" : "h-0"} `}
+			style={{
+				boxShadow:
+					"0px 6px 12px 0px rgba(92, 115, 131, 0.08), 0px 4px 8px 0px rgba(92, 115, 131, 0.08)",
+				transition: "opacity 0.3s ease-in-out",
+			}}
+			ref={wrapperRef}
+		>
+			{children}
+		</div>
+	);
 };
 
 export default Popup;
